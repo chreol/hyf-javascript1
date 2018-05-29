c806db527bb513554b96a81c43870f407782fe21
Blondel T <36702543+chreol@users.noreply.github.com>
2018-05-29 14:49:40 +0300

    WEEK2
    COmmit

---
+++ WEEK2/javascript.js
@@ -0,0 +1,150 @@
+3. Declare a variable x and initialize it with an integer, using these exact steps:
+3.1 First, declare your variable x (do not initialize it yet).
+3.2 Add a console.log statement that explains that explains in words what you think the value of x is, like in this example:
+let x=5;
+    console.log(x);
+
+
+  // TODO -> here you initialize your variable
+  console.log('the value of my variable x will be: whateverYouThinkItWillLog');
+3.3 Add a console.log statement that logs the value of x.
+3.4 Now initialize your variable x with an integer.
+3.5 Next, add a console.log statement that explains what you think the value of x is.
+3.6 Add a console.log statement that logs the value of x. Steps to be taken:
+
+  // TODO -> here you declare your variable
+const x=8;
+console.log(x);
+  console.log('the value of x will be: x');
+  // TODO -> log the actual value of x
+  // TODO -> here you initialize your variable
+  console.log('the value of x will be: x');
+  // TODO -> log value of x again
+let x="the value of x will be: whateverYouThinkItWillLog";
+console.log(x);
+console.log('the value of x will be: '+x);
+let y="code";
+console.log(typeof(y));
+console.log(y);
+
+let y="new code";
+console.log(y);
+
+let z=7.25;
+console.log(z);
+let a=0.75
+console.log(a);
+console.log(z+a);
+let b=(z+a);
+console.log(b);
+let m=[ , , ]
+console.log(typeof(m));
+let p=["cats", "birds", "dogs",];
+
+p.push("baby pig");
+console.log(p);
+let plurality=["cats", "birds", "dogs"];
+plurality.push('baby pig');
+console.log(plurality);
+
+let myString = "this is a test";
+plurality.push('myString');
+console.log(plurality);
+
+let myString = "this is a test";
+console.log(myString);
+console.log('myString length is');
+console.log(myString.length);
+let myString = "this is a test";
+console.log(myString);
+console.log('myString length is');
+console.log(myString.length);
+
+let foo = 3;
+console.log('The value of my variable foo is: ' + foo);
+let foo = 6;
+console.log('The value of my variable foo is: ' + foo);
+
+let myString = "this is a sting, this is a number";
+console.log(myString);
+myString.length
+console.log(myString.length);
+
+
+let a=1;
+let b="one";
+let c =4;
+let d = [1,2,3]
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+// console.log('I think the type of (a) is a: ' + typeof(a));
+// console.log('I think the type of (b) is a: ' + typeof(b));
+// console.log('I think the type of (c) is a: ' + typeof(c));
+// console.log('I think the type of (d) is an: ' + typeof(d));
+
+function compare(x,y){
+  if (typeof(x) === typeof(y)){
+    console.log("Same Type");
+  }else{
+    console.log("Different Type");
+  }
+}
+compare (a,c);
+compare (a,d);
+compare (a,b);
+compare (c,d);
+                                                Trouble
+let a = 1;
+let b = 1;
+let c = true;
+let d = true;
+
+function compare(x,y){
+  if (typeof x === typeof y ){
+    return "SAME TYPE";
+    }  
+   else ;{
+    return "DIFFERENT TYPE";
+  }
+}
+
+console.log(compare(a,c));
+console.log(compare(a,b));
+console.log(compare(c,d));
+console.log(compare(d,a));
+console.log(compare(d,c));
+                                        
+let x =7;
+x = x % 3;
+console.log(x); //1
+
+
+let y = 10;
+y = y % 6;
+console.log(y); //1
+
+let w =15;
+w = w % 4;
+console.log(w); //3
+
+let t=25;
+t = t %4;
+console.log(t); //1
+
+
+(6/0 === 10/0);
+
+function compare () {
+  if (typeof x === typeof y);{
+    return "the are same";
+  } 
+  Else ;{
+    return "They are different";
+  }
+}
+console.log(compare(6/0,10/0));
+
+console.log(6/0 === 10/0);
