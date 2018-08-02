c3a673d297fbdd01cdd8de87429ae5d39684b933
Blondel T <36702543+chreol@users.noreply.github.com>
2018-08-02 14:26:13 +0300

    Add files via upload

---
+++ week2/week2.js
@@ -0,0 +1,95 @@
+let booklist = {
+    Book: {
+     Author: 'Le Bruit et la Fureur',
+     Title: 'William Faukner',
+     Langage: ''
+    },
+    Book2: {
+     Author: 'A la recherche du temps perdu',
+     Title: 'Marcel Proust',
+     Language: ''
+    },
+  Book3: {
+    Author: 'Mohsin Hamid',
+    Title: 'Exit West',
+    Language: 'English'
+    },
+  Book4: {
+    Author: 'Autumn',
+    Title: 'Ali Smith',
+    Language: 'English'
+    },
+  Book5: {
+    Author: 'Naomi Alderman',
+    Title: 'The Power',
+    Langage: 'English'
+    },
+  Book6: {
+    Author: 'Min Jin Lee',
+    Title: 'Pachinko',
+    Langage: 'English'
+    },
+  Book7: {
+    Author: 'Jesmyn Ward',
+    Title: 'Sing, Unburied, Sing',
+    Langage: 'English'
+    },
+  Book8: {
+    Author: 'The Evolution of Beauty',
+    Title: 'Richard O. Prum',
+    Langage: 'English'  
+  },
+  Book9: {
+    Author: 'Ron Chernow',
+    Title: 'Grant',
+    Langage: 'English'
+    },
+  Book10: {
+    Author: 'Caroline Fraser',
+    Title: 'Prairie Fires',
+    Langage: 'English'
+  }
+  };
+  console.log(booklist);
+   
+  // console.log(booklist.entries(booklist).length);
+  // console.log(booklist.entries(booklist));
+  // console.log(booklist.entries(booklist)[0]);
+  // console.log(booklist.entries(booklist)[0][1].title);
+  // console.log(booklist.entries(booklist)[0][1].language);
+  // console.log(booklist.entries(booklist)[1]);
+  // console.log(booklist.entries(booklist)[1][1].title);
+  // console.log(booklist.entries(booklist)[1][1].language);
+  // console.log(booklist.entries(booklist)[2]);
+  // console.log(booklist.entries(booklist)[2][1].title);
+  // console.log(booklist.entries(booklist)[2][1].language);
+  // console.log(booklist.entries(booklist)[2]);
+  // console.log(booklist.entries(booklist)[3][1].title);
+  // console.log(booklist.entries(booklist)[3][1].language);
+  // console.log(booklist.entries(booklist)[3]);
+  // console.log(booklist.entries(booklist)[4]);
+  // console.log(booklist.entries(booklist)[4][1].title);
+  // console.log(booklist.entries(booklist)[4][1].language);
+  // console.log(booklist.entries(booklist)[5]);
+  // console.log(booklist.entries(booklist)[5][1].title);
+  // console.log(booklist.entries(booklist)[5][1].language);
+  // console.log(booklist.entries(booklist)[6]);
+  // console.log(booklist.entries(booklist)[6][1].title);
+  // console.log(booklist.entries(booklist)[6][1].language);
+  // console.log(booklist.entries(booklist)[7]);
+  // console.log(booklist.entries(booklist)[7][1].title);
+  // console.log(booklist.entries(booklist)[7][1].language);
+  // console.log(booklist.entries(booklist)[8]);
+  // console.log(booklist.entries(booklist)[8][1].title);
+  // console.log(booklist.entries(booklist)[8][1].language);
+  // console.log(booklist.entries(booklist)[9]);
+  // console.log(booklist.entries(booklist)[9][1].title);
+  // console.log(booklist.entries(booklist)[9][1].language);
+  // console.log(booklist.entries(booklist)[10]);
+  // console.log(booklist.entries(booklist)[10][1].title);
+  // console.log(booklist.entries(booklist)[10][1].language);
+  
+  let mybooklisT = ['Le Bruit et la Fureur_William Faukner', 
+  'A la recherche du temps perdu_Marcel Proust','Mohsin Hamid_Exit West', 'Autumn_Ali Smith', 'Naomi Alderman_The Power','Min Jin Lee_Pachinko','Jesmyn Ward_Sing Unburied Sing','The Evolution of Beauty_Richard O. Prum','Ron Chernow_Grant','Caroline Fraser_Prairie Fires']
+  
+  
